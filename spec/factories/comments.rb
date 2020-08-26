dbcbe6603dae212f8a8617061fecc2defed8bec5
saki0508 <sakiogata58@gmail.com>
2020-08-26 14:00:05 +0900

    Commentモデルのテスト

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    
+    comment {'テストコメントabc123'}
+    association :user
+    association :post
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  it 'commentが存在していれば保存できること' do
+    expect(@comment).to be_valid
+  end
+
+  it 'commentが空だと保存できないこと' do
+    @comment.comment = nil
+    @comment.valid?
+    expect(@comment.errors.full_messages).to include("Comment can't be blank")
+  end
+
 end
