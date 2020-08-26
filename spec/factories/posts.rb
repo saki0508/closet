48366ef86338c91915a6dd30d86c7c30d99ac197
saki0508 <sakiogata58@gmail.com>
2020-08-26 13:37:39 +0900

    Postモデルのテスト

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :post do
-    
+    title {'テスト相談です'}
+    text {'テスト投稿ですtest,123'}
+    association :user
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+    @post.images = fixture_file_upload('public/images/test_image1.jpg','public/images/test_image2.jpg')
+  end
+
+  it 'titleとtextとimagesが存在していれば保存できること' do
+    expect(@post).to be_valid
+  end
+
+  it 'titleとtextが存在していれば保存できること' do
+    @post.images = nil
+    expect(@post).to be_valid
+  end
+
+  it 'titleが空だと保存できないこと' do
+    @post.title = nil
+    @post.valid?
+    expect(@post.errors.full_messages).to include("Title can't be blank")
+  end
+
+  it 'textが空だと保存できないこと' do
+    @post.text = nil
+    @post.valid?
+    expect(@post.errors.full_messages).to include("Text can't be blank")
+  end
 end
