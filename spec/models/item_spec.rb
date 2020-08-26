da1146cbe953bd29d8e717727e03adfc162e83ce
saki0508 <sakiogata58@gmail.com>
2020-08-26 13:49:06 +0900

    Itemモデルのテスト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('public/images/test_image1.jpg')
+  end
+
+  it 'nameとimageがあれば保存できること' do
+    expect(@item).to be_valid
+  end
+
+  it 'nameが空だと保存できないこと' do
+    @item.name = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Name can't be blank")
+  end
+
+  it 'imageが空だと保存できないこと' do
+    @item.image = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Image can't be blank")
+  end
 end
