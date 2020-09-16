da483b73e27385209920750d2e0b8764e25012dd
saki0508 <sakiogata58@gmail.com>
2020-09-16 23:39:37 +0900

    画像投稿修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, foreign_key: "post_id", dependent: :destroy
   has_many_attached :images
-  validates :title, :text, presence: true
+  validates :title, :text, :images, presence: true
 end
